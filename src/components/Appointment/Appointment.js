53798eddb0824da4d3cf5cbd5e29159bdc3fce3d
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-04-07 20:05:01 +0600

    react-calendar formatted

--- src/components/Appointment/Appointment.js
+++ src/components/Appointment/Appointment.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
-
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './Appointment.css'
 
 const Appointment = () => {
-    
+    const [date, setDate] = useState(new Date())
     return (
-        <div>
+        <div className='container'>
             <h1>Appointment</h1>
-            
+            {console.log(date.toDateString())}
+            <Calendar
+                onChange={d => setDate(d)}
+                value={date}
+                minDate={new Date()}
+                maxDate={new Date(2020,11,31)}
+                minDetail={'year'}
+            />
         </div>
     );
 };
