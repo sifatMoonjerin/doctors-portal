d974f7e8a43e44d1d861ea138f8828d8731f673d
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-04-08 16:27:56 +0600

    services fetched from server

--- src/components/ServiceList/ServiceList.js
+++ src/components/ServiceList/ServiceList.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ServiceCard from '../ServiceCard/ServiceCard';
 import ApppointmentForm from '../AppointmentForm/ApppointmentForm';
 
 const ServiceList = (props) => {
+    const [services, setServices] = useState([])
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [appointmentInfo, setAppointmentInfo] = useState({});
     
+    useEffect(()=>{
+        fetch('http://localhost:4200/services')
+        .then(res => res.json())
+        .then(data => {
+            setServices(data)
+        })
+    },[])
+
+
     const openModal = (title,time,startTime) => {
         setAppointmentInfo({title,time,startTime,date: props.date})
         setModalIsOpen(true)
@@ -17,49 +27,65 @@ const ServiceList = (props) => {
 
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 
-    const services = [
-        {
-            title: 'Teeth Orthodontics',
-            time: '08:00 am - 09:00 am',
-            startTime: '08:00 AM'
-        },
-        {
-            title: 'Cosmetic Dentistry',
-            time: '10:00 am - 11:30 am',
-            startTime: '10:00 AM'
-        },
-        {
-            title: 'Teeth Cleaning',
-            time: '05:00 pm - 06:30 pm',
-            startTime: '05:00 PM'
-        },
-        {
-            title: 'Cavity Protection',
-            time: '07:00 pm - 08:00 pm',
-            startTime: '07:00 PM'
-        },
-        {
-            title: 'Teeth Orthodontics',
-            time: '08:00 pm - 09:00 pm',
-            startTime: '08:00 PM'
-        },
-        {
-            title: 'Cosmetic Dentistry',
-            time: '09:00 pm - 10:00 pm',
-            startTime: '09:00 AM'
-        }
-    ]
+    // const services = [
+    //     {
+    //         title: 'Teeth Orthodontics',
+    //         time: '08:00 am - 09:00 am',
+    //         startTime: '08:00 AM'
+    //     },
+    //     {
+    //         title: 'Cosmetic Dentistry',
+    //         time: '10:00 am - 11:30 am',
+    //         startTime: '10:00 AM'
+    //     },
+    //     {
+    //         title: 'Teeth Cleaning',
+    //         time: '05:00 pm - 06:30 pm',
+    //         startTime: '05:00 PM'
+    //     },
+    //     {
+    //         title: 'Cavity Protection',
+    //         time: '07:00 pm - 08:00 pm',
+    //         startTime: '07:00 PM'
+    //     },
+    //     {
+    //         title: 'Teeth Orthodontics',
+    //         time: '08:00 pm - 09:00 pm',
+    //         startTime: '08:00 PM'
+    //     },
+    //     {
+    //         title: 'Cosmetic Dentistry',
+    //         time: '09:00 pm - 10:00 pm',
+    //         startTime: '09:00 AM'
+    //     }
+    // ]
+
+    // const updateServices = () => {
+    //     fetch('http://localhost:4200/addServices',{
+    //         method: 'POST',
+    //         headers: {
+    //           'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(services)
+    //       })
+    //       .then(res => res.json())
+    //       .then(data => {
+    //           console.log('post successful');
+    //       })
+    // }
 
     return (
         <div>
             <h3 className="text-info m-4">
                 Available Appointments on {props.date.toLocaleDateString(undefined, options)}
             </h3>
+            {/* <button onClick={updateServices}>add services</button> */}
             <div className="row">
                 {
                     services.map(el => <div 
                     className="col-md-4 d-flex justify-content-center">
-                        <ServiceCard 
+                        <ServiceCard
+                            key={el._id} 
                             title={el.title}
                             time={el.time}
                             startTime={el.startTime}
